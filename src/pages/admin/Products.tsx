6f2a905b27d616bc491399441aad1967442b14d8
HIDEOMI <mm.i.lub@gmail.com>
2024-10-06 15:24:35 +0900

    Update Products.tsx

--- src/pages/admin/Products.tsx
+++ src/pages/admin/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Select from 'react-select';
 import { Product, Material, MaterialInProduct } from '../../types/types';
 import { getAllProducts, addProduct, updateProduct, deleteProduct } from '../../services/products';
@@ -16,6 +16,7 @@ const Products: React.FC = () => {
     const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
     // const [selectedCategory, setSelectedCategory] = useState<string>('All');  // 選択されたカテゴリ
     const [selectedCategory, setSelectedCategory] = useState<string>('ジン');  // 選択されたカテゴリ
+    const [isReady, setIsReady] = useState<boolean>(false);  // Readyを非表示にするかどうか
     const [selectedMaterials, setSelectedMaterials] = useState<MaterialInProduct[]>([]);
     const [formProduct, setFormProduct] = useState<Product>({
         id: "",
@@ -52,6 +53,9 @@ const Products: React.FC = () => {
         value: material.id,
         label: material.name,
     }));
+
+    const formRef = useRef<HTMLDivElement>(null);  // フォームへの参照を作成
+    const productRefs = useRef<{ [key: string]: HTMLLIElement | null }>({});  // 商品ごとの参照を格納
 
 
     useEffect(() => {
@@ -190,20 +194,39 @@ const Products: React.FC = () => {
         formProduct.price = 50 + prices.reduce((sum, element) => sum + element, 0);
 
         if (isEditing) {
+            // 商品の更新処理
+
             const { id, ...updateData } = formProduct;
             updateData.materials = selectedMaterials;
+
+            // 材料のquntityが1より大きい場合、alreadyステータスをReadyに変更
+            if (selectedMaterials.some(m => m.quantity > 0)) {
+                updateData.already = "Ready";
+            }
+
             await updateProduct(id, updateData);
-            showMessage('商品を更新しました');
+
+            // 更新完了後に、編集していた商品にスクロール
+            productRefs.current[formProduct.id]?.scrollIntoView();
+            window.confirm('商品を更新しました');
         } else {
+            // 新規追加の場合の処理
+
             formProduct.materials = selectedMaterials;
             await addProduct(formProduct);
-            showMessage('商品を追加しました');
+            window.confirm('商品を追加しました');
         }
 
         resetForm();
         const allProductsData = await getAllProducts();
         setAllProducts(allProductsData);
-        setFilteredProducts(allProductsData);
+
+        if (isReady) {
+            const filtered = allProducts.filter((product) => product.already !== 'Ready');
+            setFilteredProducts(filtered);
+        } else {
+            setFilteredProducts(allProductsData);
+        }
     };
 
     /** キャンセルボタンを押したときのハンドラ */
@@ -282,6 +305,17 @@ const Products: React.FC = () => {
         }
     };
 
+    /** Readyを非表示にするハンドラ */
+    const handleReadyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setIsReady(e.target.checked);
+        if (e.target.checked) {
+            const filtered = allProducts.filter((product) => product.already !== 'Ready');
+            setFilteredProducts(filtered);
+        } else {
+            setFilteredProducts(allProducts);
+        }
+    };
+
     /** 材料の値を変更したときに変更内容を保持するハンドラ */
     const handlePendingUpdate = async (updateInfo: { [key: string]: any; id: string; }) => {
         setPendingUpdates([...pendingUpdates, updateInfo]);
@@ -290,9 +324,14 @@ const Products: React.FC = () => {
 
     /** 商品を編集するハンドラ */
     const handleEditProduct = (product: Product) => {
+        setFormProduct(product);  // 編集対象の商品をフォームにセット
+        setIsEditing(true);  // 編集モードに切り替え
+
         setFormProduct(product);  // 編集モードで既存データをセット
         setSelectedMaterials(product.materials);
-        setIsEditing(true);
+
+        // 編集フォームにスクロール
+        formRef.current?.scrollIntoView();
     };
 
     /** 商品を削除するハンドラ */
@@ -314,122 +353,125 @@ const Products: React.FC = () => {
     return (
         <div>
             <h2>商品管理</h2>
+            <div ref={formRef}>  {/* 編集フォームに参照を設定 */}
+                <h3>商品登録 / 編集</h3>
+                {message && <p style={{ color: 'red' }}>{message}</p>} {/* メッセージを表示 */}
+                {error && <p style={{ color: 'red' }}>{error}</p>} {/* エラーメッセージを表示 */}
 
-            <h3>商品登録 / 編集</h3>
-            {message && <p style={{ color: 'red' }}>{message}</p>} {/* メッセージを表示 */}
-            {error && <p style={{ color: 'red' }}>{error}</p>} {/* エラーメッセージを表示 */}
+                <div>
+                    <label>商品名: </label>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="商品名"
+                        value={formProduct.name}
+                        onChange={handleInputChange}
+                    />
+                    <br />
+                    <label>説明: </label>
+                    <textarea
+                        name="description"
+                        placeholder="説明"
+                        value={formProduct.description}
+                        onChange={handleInputChange}
+                    />
+                    <br />
+                    <label>カテゴリ: </label>
+                    <select
+                        name="categories"
+                        multiple
+                        value={formProduct.categories}
+                        onChange={handleSelectChange}
+                    >
+                        {categoriesList.map(category => (
+                            <option key={category} value={category}>
+                                {category}
+                            </option>
+                        ))}
+                    </select>
+                    <br />
+                    <label>ベース: </label>
+                    <select
+                        name="bases"
+                        multiple
+                        value={formProduct.bases}
+                        onChange={handleSelectChange}
+                    >
+                        {baseList.map(base => (
+                            <option key={base} value={base}>
+                                {base}
+                            </option>
+                        ))}
+                    </select>
+                    <br />
+                    <label>色: </label>
+                    <input
+                        type="text"
+                        name="color"
+                        placeholder="色"
+                        value={formProduct.color}
+                        onChange={handleInputChange}
+                    />
+                    <br />
+                    <label>アルコール度数: </label>
+                    <input
+                        type="number"
+                        name="alc"
+                        placeholder="アルコール度数"
+                        value={formProduct.alc}
+                        onChange={handleInputChange}
+                    />
+                    <br />
+                    <label>レシピ: </label>
+                    <textarea
+                        name="recipe"
+                        placeholder="レシピ"
+                        value={formProduct.recipe}
+                        onChange={handleInputChange}
+                        rows={7} // ここで行数を指定
+                        style={{ width: '80%', fontSize: '1rem' }} // 幅とフォントサイズを調整
+                    />
+                    <br />
+                    <label>画像URL: </label>
+                    <input
+                        type="text"
+                        name="imageUrl"
+                        placeholder="画像URL"
+                        value={formProduct.imageUrl}
+                        onChange={handleInputChange}
+                    />
 
-            <div>
-                <label>商品名: </label>
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="商品名"
-                    value={formProduct.name}
-                    onChange={handleInputChange}
-                />
-                <br />
-                <label>説明: </label>
-                <textarea
-                    name="description"
-                    placeholder="説明"
-                    value={formProduct.description}
-                    onChange={handleInputChange}
-                />
-                <br />
-                <label>カテゴリ: </label>
-                <select
-                    name="categories"
-                    multiple
-                    value={formProduct.categories}
-                    onChange={handleSelectChange}
-                >
-                    {categoriesList.map(category => (
-                        <option key={category} value={category}>
-                            {category}
-                        </option>
-                    ))}
-                </select>
-                <br />
-                <label>ベース: </label>
-                <select
-                    name="bases"
-                    multiple
-                    value={formProduct.bases}
-                    onChange={handleSelectChange}
-                >
-                    {baseList.map(base => (
-                        <option key={base} value={base}>
-                            {base}
-                        </option>
-                    ))}
-                </select>
-                <br />
-                <label>色: </label>
-                <input
-                    type="text"
-                    name="color"
-                    placeholder="色"
-                    value={formProduct.color}
-                    onChange={handleInputChange}
-                />
-                <br />
-                <label>アルコール度数: </label>
-                <input
-                    type="number"
-                    name="alc"
-                    placeholder="アルコール度数"
-                    value={formProduct.alc}
-                    onChange={handleInputChange}
-                />
-                <br />
-                <label>レシピ: </label>
-                <textarea
-                    name="recipe"
-                    placeholder="レシピ"
-                    value={formProduct.recipe}
-                    onChange={handleInputChange}
-                />
-                <br />
-                <label>画像URL: </label>
-                <input
-                    type="text"
-                    name="imageUrl"
-                    placeholder="画像URL"
-                    value={formProduct.imageUrl}
-                    onChange={handleInputChange}
-                />
+                    {/* 材料の選択 */}
+                    <br />
+                    <br />
+                    {/* 材料のサジェスト可能なプルダウン */}
+                    <label>材料を選択</label>
+                    <Select
+                        isMulti  // 複数選択可能
+                        options={materialOptions}  // 材料の選択肢を表示
+                        value={selectedMaterials.map(m => ({ value: m.id, label: m.name }))}  // 選択された材料を反映
+                        onChange={handleSelectMaterial}  // 選択変更時の処理
+                    />
 
-                {/* 材料の選択 */}
-                <br />
-                <br />
-                {/* 材料のサジェスト可能なプルダウン */}
-                <label>材料を選択</label>
-                <Select
-                    isMulti  // 複数選択可能
-                    options={materialOptions}  // 材料の選択肢を表示
-                    value={selectedMaterials.map(m => ({ value: m.id, label: m.name }))}  // 選択された材料を反映
-                    onChange={handleSelectMaterial}  // 選択変更時の処理
-                />
+                    {/* 使用する材料のリスト */}
+                    <h4>使用する材料</h4>
+                    <ul>
+                        {selectedMaterials.map((material, index) => (
+                            <li key={material.id}>
+                                {material.name} - 量:
+                                <input
+                                    type="number"
+                                    value={material.quantity || 0}
+                                    onChange={(e) => handleQuantityChange(index, Number(e.target.value))}  // 量を変更
+                                />
+                            </li>
+                        ))}
+                    </ul>
 
-                {/* 使用する材料のリスト */}
-                <h4>使用する材料</h4>
-                <ul>
-                    {selectedMaterials.map((material, index) => (
-                        <li key={material.id}>
-                            {material.name} - 量:
-                            <input
-                                type="number"
-                                value={material.quantity || 0}
-                                onChange={(e) => handleQuantityChange(index, Number(e.target.value))}  // 量を変更
-                            />
-                        </li>
-                    ))}
-                </ul>
+                    <button onClick={handleSubmit}>{isEditing ? '商品を更新' : '商品を追加'}</button>
+                    <button onClick={handleCancel}>キャンセル</button>
 
-                <button onClick={handleSubmit}>{isEditing ? '商品を更新' : '商品を追加'}</button>
-                <button onClick={handleCancel}>キャンセル</button>
-
+                </div>
             </div>
 
             <h2>商品リスト</h2>
@@ -446,6 +488,10 @@ const Products: React.FC = () => {
                     ))}
                 </select>
 
+                <br />
+                <label>Readyを非表示にする場合はチェック </label>
+                <input type="checkbox" checked={isReady} onChange={handleReadyChange} />
+
                 {loading ? (
                     <p>読み込み中...</p>
                 ) : (
@@ -454,33 +500,34 @@ const Products: React.FC = () => {
                             <p>該当する商品がありません。</p>
                         ) : (
                             <>
-                                <ProductTable
+                                {/* <ProductTable
                                     products={filteredProducts}
                                     handlePendingUpdate={handlePendingUpdate}
                                     handleDeleteRow={handleDeleteRow}
-                                />
-                                {/* <ul> */}
-                                {/* {filteredProducts.map(product => ( */}
-                                {/* <li key={product.id}> */}
-                                {/* <> */}
-                                {/* {console.log(product.name)} */}
-                                {/* {console.log(product.id)} */}
-                                {/* <h4>{product.name}</h4>
-                                            値段: ¥ {product.price.toLocaleString()} <br />
-                                            カテゴリ: {product.categories.join(', ')} <br />
-                                            ベース: {product.bases.join(', ')} <br />
-                                            アルコール度数: {product.alc}% <br />
-                                            色: {product.color} <br />
-                                            材料: {product.materials.map(m => m.name).join(', ')} <br />
-                                            在庫: {product.isAvailable ? 'あり' : 'なし'} <br />
-                                            レシピ: {product.recipe} <br />
-                                            準備完了： {product.already} <br />
-                                            <button onClick={() => handleEditProduct(product)}>編集</button>
-                                            <button onClick={() => handleDeleteRow(product.id)}>削除</button> */}
-                                {/* </> */}
-                                {/* </li> */}
-                                {/* ))} */}
-                                {/* </ul> */}
+                                /> */}
+                                <ul>
+                                    {filteredProducts.map(product => (
+                                        <li
+                                            key={product.id}
+                                            ref={el => (productRefs.current[product.id] = el)}  // 各商品に対して参照を設定
+                                        >
+                                            <>
+                                                <h4>{product.name}</h4>
+                                                値段: ¥ {product.price.toLocaleString()} <br />
+                                                カテゴリ: {product.categories.join(', ')} <br />
+                                                ベース: {product.bases.join(', ')} <br />
+                                                アルコール度数: {product.alc}% <br />
+                                                色: {product.color} <br />
+                                                材料: {product.materials.map(m => m.name).join(', ')} <br />
+                                                在庫: {product.isAvailable ? 'あり' : 'なし'} <br />
+                                                レシピ: {product.recipe} <br />
+                                                準備完了： {product.already} <br />
+                                                <button onClick={() => handleEditProduct(product)}>編集</button>
+                                                <button onClick={() => handleDeleteRow(product.id)}>削除</button>
+                                            </>
+                                        </li>
+                                    ))}
+                                </ul>
                             </>
                         )}
                     </div>
